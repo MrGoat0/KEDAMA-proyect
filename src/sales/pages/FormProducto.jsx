44676dc000b9447c141ef326f5024f60445ce060
Kevin Barrera <kbarrerac@unal.edu.co>
2021-10-01 12:18:30 -0500

    Intento de modal Edit

--- src/sales/pages/FormProducto.jsx
+++ src/sales/pages/FormProducto.jsx
@@ -1,9 +1,16 @@
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
-import { Button } from "react-bootstrap";
+import { Button, Modal, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Producto = ({ producto }) => {
   /* const handleClick = () => {
     <Edit />;
   }; */
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <tr className="text-center">
       <th className="text-left">{producto.id}</th>
@@ -11,15 +18,53 @@ const Producto = ({ producto }) => {
       <th>{producto.price}</th>
       <th>{producto.location}</th>
       <th>
-        <Button href="/categories/sales/edit" type="button" variant="primary">
-          <AiFillEdit />
-          Editar Venta
+        {/* <Button href="/categories/sales/edit" type="button" variant="primary">
+          <AiFillEdit />- Editar Venta
+          <Edit producto={producto} />
+        </Button> */}
+        <Button onClick={handleShow} variant="primary">
+          <AiFillEdit />- Editar Venta
         </Button>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Editar valores</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>ID-{producto.id}</Form.Label>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Producto</Form.Label>
+                <Form.Control type="text" placeholder={producto.producto} />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Precio</Form.Label>
+                <Form.Control type="text" placeholder={producto.price} />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Tienda (Localización)</Form.Label>
+                <Form.Control type="text" placeholder={producto.location} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cerrar
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Guardar cambios
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </th>
       <th>
         <Button variant="primary" href="#">
-          <AiFillDelete />
-          Borrar venta
+          <AiFillDelete />- Borrar venta
         </Button>
       </th>
     </tr>
--- src/sales/pages/Sales.jsx
+++ src/sales/pages/Sales.jsx
@@ -2,32 +2,28 @@ import React from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import Header from "../../shared/Header.jsx";
 import SalesList from "./SalesList";
+import Footer from "../../shared/Footer.jsx";
+import "../../styles/category.css";
 const Sales = () => {
   const handleClick = () => {
     return <SalesList />;
   };
   return (
-    <div>
+    <div className="container-Category ">
       <Header />
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/categories">Home</Navbar.Brand>
           <Nav className="me-auto">
-            <Button
-              onClick={handleClick}
-              type="button"
-              variant="primary"
-              /* className="float-end" */
-            >
-              Listar
-            </Button>
             {/* <Nav.Link href="#home">Listar ventas</Nav.Link>
-             <Nav.Link href="#features">Buscar</Nav.Link>
+            <Nav.Link href="#features">Buscar</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
       <SalesList />
+
+      <Footer />
     </div>
   );
 };
