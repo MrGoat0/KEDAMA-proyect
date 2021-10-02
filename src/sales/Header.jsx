5d3600c2ff696ca74789b939ee238a09dbe87a97
Diego Marin <dimarka14@gmail.com>
2021-10-02 09:14:18 -0500

    commit ventas

---
+++ src/sales/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import '../styles/shared.css';
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1> SISTEMA DE INFORMACIÓN DE VENTAS </h1>
+    </div>
+
+  )
+
+
+}
+export default Header
