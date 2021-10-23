d5d6ea4a1417f6abda03a4e86d3af9fdfecd9913
MateoCardonaRincon <mcardonari@unal.edu.co>
2021-10-22 19:27:08 -0500

    changes by autoformat on save

--- src/sales/components/TableBody.jsx
+++ src/sales/components/TableBody.jsx
@@ -5,17 +5,17 @@ import TableRow from "./TableRow";
 //     }
 // }
 const TableBody = (props) => {
-    const {recordsData,type} = props;
-    if(typeof recordsData !== "undefined"){
-        return(
+    const { recordsData, type } = props;
+    if (typeof recordsData !== "undefined") {
+        return (
             <tbody>
-                {recordsData.map((row)=>(
+                {recordsData.map((row) => (
                     <TableRow row={row} type={type} />
                 ))}
-            {/* {iterateTrough(recordsData)} */}
+                {/* {iterateTrough(recordsData)} */}
             </tbody>
         )
-    }else{ return null}
+    } else { return null }
 }
 
 export default TableBody;
