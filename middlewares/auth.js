7d0934f1374fba26e1601265b7269023066aedcf
MateoCardonaRincon <mcardonari@unal.edu.co>
2021-10-28 11:10:04 -0500

    trying to fix fetchRole error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 // in the parameter req we send the authentication token
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
-  const decodedToken = await jwt.decode(token);
+  const decodedToken = jwt.decode(token);
   if (decodedToken) {
     req.userData = { email: decodedToken.email, name: decodedToken.name };
     next();
