a271b194534ab0ffd556ea8f1508eaaea08854ea
MateoCardonaRincon <mcardonari@unal.edu.co>
2021-10-28 11:13:30 -0500

    Trying to fix fetch error

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ const salesRoutes = require("./routes/sales");
 const productsRoutes = require("./routes/products");
 const usersRoutes = require("./routes/users");
 
+// connection to database
+const connectionString = process.env.MONGODB_CONNECTION;
+mongoose.connect(connectionString).then(() => {
+  console.log("Connected to MongoDB!");
+});
+
 // middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-
-// connection to database
-const connectionString = process.env.MONGODB_CONNECTION;
-mongoose.connect(connectionString).then(() => {
-  console.log("Connected to MongoDB!");
-}); 
 
 // define api routes
 app.use("/api/sales", salesRoutes);
