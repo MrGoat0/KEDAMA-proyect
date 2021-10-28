402477437cda3b14e91f301cbc8677df1edefe16
MateoCardonaRincon <mcardonari@unal.edu.co>
2021-10-28 12:56:10 -0500

    Trying to fix fetch error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 // in the parameter req we send the authentication token
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
-  const decodedToken = jwt.decode(token);
+  const decodedToken = await jwt.decode(token);
   if (decodedToken) {
     req.userData = { email: decodedToken.email, name: decodedToken.name };
     next();
